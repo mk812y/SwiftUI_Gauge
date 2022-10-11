612ba9c78dbac3164d532981791c381fc800d7b7
mk812y <mk812y@gmail.com>
2022-10-11 14:08:02 +0300

    Initial Commit

---
+++ SwiftUI_Gauge/SwiftUI_GaugeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_GaugeApp.swift
+//  SwiftUI_Gauge
+//
+//  Created by Михаил Куприянов on 11.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_GaugeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
