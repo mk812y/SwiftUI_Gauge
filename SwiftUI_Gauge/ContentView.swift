cb558b191bfce820361bc02025c61113e308e6de
mk812y <mk812y@gmail.com>
2022-10-11 16:42:45 +0300

    запомним

--- SwiftUI_Gauge/ContentView.swift
+++ SwiftUI_Gauge/ContentView.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 struct ContentView: View {
     @State var progress = 0.7
-    @State private var currentSpeed = 75.0
+    @State private var currentSpeed = 100.0
     
     var body: some View {
         VStack {
@@ -50,6 +50,13 @@ struct ContentView: View {
             }
             .tint(.red)
             addGaugeStyle
+            Gauge(value: currentSpeed, in: 0...200) {
+                   Image(systemName: "gauge.medium")
+                       .font(.system(size: 50.0))
+               } currentValueLabel: {
+                   Text("\(currentSpeed.formatted(.number))")
+               }
+               .gaugeStyle(SpeedometerGaugeStyle())
         }
         .padding()
     }
@@ -118,4 +125,52 @@ struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
+}
+
+struct SpeedometerGaugeStyle: GaugeStyle {
+    private var purpleGradient = LinearGradient(
+        gradient: Gradient(colors: [
+            Color(.red), Color(.blue)
+        ]),
+        startPoint: .trailing,
+        endPoint: .leading)
+    
+    func makeBody(configuration: Configuration) -> some View {
+        ZStack {
+            Circle()
+                .foregroundColor(Color(.systemGray6))
+            Circle()
+                .trim(from: 0, to: 0.75 * configuration.value)
+                .stroke(purpleGradient, lineWidth: 20)
+                .rotationEffect(.degrees(135))
+            Circle()
+                .trim(from: 0, to: 0.75)
+                .stroke(Color(.black), style: StrokeStyle(
+                    lineWidth: 10,
+                    lineCap: .butt,
+                    lineJoin: .round,
+                    dash: [1, 34],
+                    dashPhase: 0.0
+                ))
+                .rotationEffect(.degrees(135))
+            
+            VStack {
+                configuration.currentValueLabel
+                    .font(.system(
+                        size: 80,
+                        weight: .bold,
+                        design: .rounded
+                    ))
+                    .foregroundColor(.gray)
+                Text("km/h")
+                    .font(.system(.body, design: .rounded))
+                    .bold()
+                    .foregroundColor(.gray)
+            }
+        }
+        .frame(width: 300, height: 300)
+    }
+    
+    
+    
 }
